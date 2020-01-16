a3c49897824e1e33a2ccb8fb528338b6fd1fcc63
A. Ahmed <paris@powerbot.org>
2020-01-16 15:04:40 +0000

    Next version

--- src/main/java/org/powerbot/bot/ContextClassLoader.java
+++ src/main/java/org/powerbot/bot/ContextClassLoader.java
@@ -2,7 +2,7 @@ package org.powerbot.bot;
 
 import org.powerbot.script.*;
 
-@Script.Manifest(name = "RSBot", description = "powerbot.org", version = "8.0.8")
+@Script.Manifest(name = "RSBot", description = "powerbot.org", version = "8.0.9")
 public abstract class ContextClassLoader extends ClassLoader {
 	private final ClientContext ctx;
 
